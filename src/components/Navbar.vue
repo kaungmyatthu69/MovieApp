cb56f81a99e0aa7b41f4d03ce379ccc167ecf0b7
kaungmyatthu <kaungmyatthu692017@gmail.com>
2023-07-26 17:13:13 +0630

    developed navbar and home page hero section

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-blue-400 flex py-5 justify-between items-center ">
+<bars4-icon class="w-8 h-8 text-white" />
+    <h1>The Movie DB</h1>
+    <div class="flex">
+      <div class="relative">
+        <bell-icon class="w-6 h-6 text-white"/>
+        <span class="absolute inline-flex items-center w-4 h-4 justify-center rounded-full  text-xs bg-red-600  text-white -top-2 -right-1">4</span>
+      </div>
+      <user-icon class="text-white w-6 h-6 " />
+      <MagnifyingGlassIcon class="text-blue-800 font-bold w-6 h-6" />
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import {Bars4Icon} from "@heroicons/vue/24/outline";
+import {BellIcon,MagnifyingGlassIcon,UserIcon} from  "@heroicons/vue/24/solid"
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container mx-auto ">
+    <div class=" bgImage backdrop-blur w-full  h-full bg-cover bg-center ">
+        <div class="text-white px-10 flex flex-col gap-10 backdrop-blur-sm md:py-40 py-20   ">
+          <div class="flex flex-col gap-3">
+            <h1 class=" text-6xl">Welcome.</h1>
+            <h2 class="text-3xl">Millions of movies, TV shows and people to discover ,Explore now.</h2>
+          </div>
+
+          <div class="flex justify-between bg-white rounded-full  ">
+            <input placeholder="Search... " class="w-full text-black  px-5 py-3 focus:ring-0 focus:outline-0  border-none rounded-full"/>
+            <button class="bg-blue-400  px-5  text-white rounded-full ">Search</button>
+          </div>
+        </div>
+
+
+
+    </div>
+
+  </div>
+
+</template>
+
+
+<style>
+
+.bgImage{
+  background-image: url("src/assets/images/image1.jpg");
+}
+
+</style>
+
